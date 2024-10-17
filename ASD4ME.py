fe8cc191bdb3ade649ee0405823bb0b656ee2912
AddyVeerendra <addyveerendra@gmail.com>
2024-10-17 11:05:01 -0400

    Addition of token validation
    JWT tokens and cookies now present to validate sessions. Next step is to observe and debug the "session" token created

--- ASD4ME.py
+++ ASD4ME.py
@@ -1,174 +1,138 @@
-"""
-ASD4ME.py is the main file of the project. It contains the main code for the project, and also has the blueprint
-for the market. The blueprint is registered with the app, and the app is run on the server. This file is where
-database connections are initialized, and where users first interact with the website. The program contains these
-functions:
-        - home(): This function renders the index.html template, the homepage of the website
-        - login(): This function renders the login.html template, and allows users to log in to the website using forms.
-        - logout(): This function logs the user out of the website.
-        - signup(): This function renders the signup.html template, allowing users to sign up for the website via forms.
-        - load_user(): This function loads the user's id from the database.
-        - app.run(): This function runs the app on the server.
-"""
-
-# os import to retrieve database path
 import os
+import logging
+from datetime import timedelta
 
-# General flask imports
-from flask import Flask, render_template, url_for, redirect
+from flask import Flask, render_template, url_for, redirect, jsonify, request, make_response
 from flask_login import login_user, login_required, logout_user
 from flask_wtf import CSRFProtect
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import InputRequired, Length
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity, set_access_cookies
+from flask_cors import CORS
 
-# Import the market blueprint
 from Market import market_bp
-# Import extensions such as db and bcrypt from extensions.py
 from extensions import db, bcrypt, login_manager, migrate
-# Import the User model from models.py
 from models import User
-
-'''
-Things to know:
-@login_required: Checks if the user is logged in. If not, the page does not open.
-FlaskForm: Type of form defined by flask_wtf in imports
-StringField: Field for users to enter text
-SubmitField: Button that receives submit input
-InputRequired: Checks if the field is filled
-Length: Checks if the length of the field is within a certain range
-NumberRange: Checks if the number is within a certain range
-'''
 
 # Get the path of the database
 file_path = os.path.abspath(os.getcwd()) + "/Database.db"
 
 # Initialize the app
 app = Flask(__name__)
+
 # Set the app configuration
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + file_path
-# Set the app configuration
 app.config['SECRET_KEY'] = 'Study4Money'
-# Initialize the database
+app.config['JWT_SECRET_KEY'] = 'Token4TheW1N'
+app.config['JWT_ACCESS_TOKEN_EXPIRES'] = timedelta(days=1)
+
+# Initialize the database and other extensions
 db.init_app(app)
-# Initialize the migration system
 migrate.init_app(app, db)
-# Initialize the CSRF protection
 csrf = CSRFProtect(app)
-
-# Initialize bcrypt
 bcrypt.init_app(app)
-# Initialize the login manager
 login_manager.init_app(app)
-# Set the login view
 login_manager.login_view = 'login'
-
-# Register the market blueprint with the app to gain access to market.py
+jwt = JWTManager(app)
+CORS(app)
 app.register_blueprint(market_bp, url_prefix='/market')
+
+# Set up logging
+logging.basicConfig(level=logging.INFO)
+
+app.config['JWT_TOKEN_LOCATION'] = ['cookies']
+app.config['JWT_COOKIE_SECURE'] = True
+app.config['JWT_COOKIE_CSRF_PROTECT'] = True
 
 
 @login_manager.user_loader
 def load_user(user_id):
-    """
-    This function loads the user's id by querying the database and returning the id when found.
-    """
-    # return the user's database id
+    logging.info(f"Loading user with id: {user_id}")
     return User.query.get(int(user_id))
 
 
 class SignupForm(FlaskForm):
-    """
-    Form for users to create an account for the website by entering a username and password, and submitting the form.
-    """
-    # username: Field for users to enter their username
     username = StringField(validators=[InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Username"})
-    # password: Field for users to enter their password
     password = PasswordField(validators=[InputRequired(), Length(min=8, max=20)], render_kw={"placeholder": "Password"})
-    # submit: Button for users to submit the form
     submit = SubmitField('Sign Up')
 
 
 class LoginForm(FlaskForm):
-    """
-    Form for users to log in to the website by entering a username and password, and submitting the form.
-    """
-    # username: Field for users to enter their username
     username = StringField(validators=[InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Username"})
-    # password: Field for users to enter their password
     password = PasswordField(validators=[InputRequired(), Length(min=8, max=20)], render_kw={"placeholder": "Password"})
-    # submit: Button for users to submit the form
     submit = SubmitField('Login')
 
 
 @app.route('/')
 def home():
-    """
-    This function renders the index.html template, the homepage of the website.
-    """
-    # Render the index.html template
     return render_template('index.html')
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    """
-    This function renders the login.html template, and allows users to log in to the website using forms.
-    """
-    # Initialize LoginForm
     form = LoginForm()
-    # Check if the form is validated
+
+    if request.method == 'GET':
+        return render_template('login.html', form=form)
+
     if form.validate_on_submit():
-        # Query the database for the user
         user = User.query.filter_by(username=form.username.data).first()
-        # Check if the user exists and the password is correct
+        logging.info(f"Attempting login for user: {form.username.data}")
+
         if user and bcrypt.check_password_hash(user.password, form.password.data):
-            # Log the user in, starting their session
-            login_user(user)
-            # Redirect to the market blueprint's home
-            return redirect(url_for('market_bp.market_home'))
-    # Render the login.html template
-    return render_template('login.html', form=form)
+            logging.info(f"Password matched for user: {user.username}")
+            access_token = create_access_token(identity=user.id)
+            logging.info(f"Access token created for user: {user.username}")
+
+            response = make_response(jsonify(success=True))
+            set_access_cookies(response, access_token)
+            logging.info(f"Access token set in cookies for user: {user.username}")
+
+            return response
+
+        logging.error(f"Login failed for user: {form.username.data}")
+    return jsonify(success=False, message="Invalid credentials"), 401
 
 
 @app.route('/logout', methods=['GET', 'POST'])
 @login_required
 def logout():
-    """
-    This function logs the user out of the website, ending their session.
-    """
-    # Log the user out, ending their session
+    logging.info(f"User logging out: {get_jwt_identity()}")
     logout_user()
-    # Redirect to the login page
     return redirect(url_for('login'))
 
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
-    """
-    This function renders the signup.html template, allowing users to create an account for the website via forms.
-    """
-    # Initialize SignupForm
     form = SignupForm()
-    # Check if the form is validated
+
     if form.validate_on_submit():
-        # Create a new hashed password
         hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
-        # Create a new user with the username and hashed password
         new_user = User(username=form.username.data, password=hashed_password)
-        # Add the new user to the database
         db.session.add(new_user)
-        # Commit the changes to the database
         db.session.commit()
-        # Redirect to the login page
+        logging.info(f"New user created: {new_user.username}")
         return redirect(url_for('login'))
-    # Render the signup.html template
+
     return render_template('signup.html', form=form)
 
 
-# Run the app on the server
+@app.route('/some_secure_endpoint', methods=['GET'])
+@jwt_required()
+def secure_endpoint():
+    current_user = get_jwt_identity()
+    logging.info(f"Accessing secure endpoint by user: {current_user}")
+    return jsonify(success=True, message=f"Hello, user {current_user}!")
+
+
+@app.errorhandler(Exception)
+def handle_exception(e):
+    logging.error(f"Error occurred: {str(e)}")
+    return jsonify(success=False, message=str(e)), 500
+
+
 if __name__ == '__main__':
-    # Create all the tables in the database
     with app.app_context():
         db.create_all()
-        # Run the app on the server
     app.run(host='0.0.0.0', port=5000)
--- Market.py
+++ Market.py
@@ -1,52 +1,24 @@
-"""
-Market.py is a blueprint that contains all the routes and functions for the market application. It is accessed by
-ASD4ME.py when necessary, and contains many market operations such as purchasing and sharing. The functions in this
-file are:
-        - market_home(): Home page of the market application. Displays basic user info such as wallet balance, and
-        allows navigation to other parts of the website. Also displays all study guides available for purchase.
-        - share(): Page for users to share study guides. Users enter the necessary information to share a study guide in
-        share.html
-        - account_home(): account_home is the user's account page, which displays account info such as wallet balance
-        and cart. This function gives users the option to remove study guides, finalize their cart purchases, and view
-        items in their inventory.
-        - finalize_purchase(): finalize_purchase allows users to finish adding the items in their cart to their
-        inventory, and accordingly checks and subtracts from wallet balance. If the wallet_balance is high enough, the
-        transaction is completed and items are moved to their inventory. Otherwise, it does not go through.
-        - admin_home(): Admin home page. Displays pending study guides and allows admin to approve or reject them.
-        - search(): Page for users to search study guides. Users enter a string in searchbar.html and the string is
-        retrieved and stored
-        - results(): results allows users to view the results of their search query, and add study guides to their cart
-        - logout(): logout logs the user out of the website.
-"""
-
 # General flask imports
+import logging
+
 from flask import Blueprint, redirect, url_for, request
 from flask import render_template
-from flask_login import login_required, current_user, logout_user
+from flask_jwt_extended import jwt_required, get_jwt_identity, unset_jwt_cookies
+from flask_login import logout_user
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, IntegerField
-from wtforms.validators import DataRequired
-from wtforms.validators import InputRequired, Length, NumberRange
+from wtforms.validators import DataRequired, InputRequired, Length, NumberRange
 
 # Database imports
 from extensions import db
 # Model imports
 from models import StudyGuide, PendingStudyGuide, Cart, CartItem, Inventory, User
 
-'''
-Things to know:
-@login_required: Checks if the user is logged in. If not, the page does not open.
-FlaskForm: Type of form defined by flask_wtf in imports
-StringField: Field for users to enter text
-SubmitField: Button that receives submit input
-InputRequired: Checks if the field is filled
-Length: Checks if the length of the field is within a certain range
-NumberRange: Checks if the number is within a certain range
-'''
-
 # Blueprint for the market application (Accessed by ASD4ME.py and HTML templates)
 market_bp = Blueprint('market_bp', __name__)
 
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 class ShareForm(FlaskForm):
     """
@@ -90,34 +62,25 @@ class SearchForm(FlaskForm):
     submit = SubmitField('Search')
 
 
+
 @market_bp.route('/')
-@login_required
+@jwt_required()
 def market_home():
-    """
-    Home page of the market application. Displays basic user info such as wallet balance, and allows navigation to other
-    parts of the website. Also displays all study guides available for purchase.
-    """
-    # Get the current user
-    user = current_user
-    # Get all study guides
+    user_id = get_jwt_identity()
+    logging.info(f"JWT Identity: {user_id}")
+    user = User.query.filter_by(id=user_id).first()
     items = StudyGuide.query.all()
-    # Render market.html for the users to see the market home page
     return render_template('market.html', user=user, items=items)
 
 
+
 @market_bp.route('/share', methods=['GET', 'POST'])
-@login_required
+@jwt_required()
 def share():
-    """
-    Page for users to share study guides. Users enter the necessary information to share a study guide in share.html
-    """
-    # Initialize form
+    user = User.query.filter_by(id=get_jwt_identity()).first()
     form = ShareForm()
-    # Autofill creator field with username of account
-    form.Creator.data = current_user.username
-    # Check for form validation
+    form.Creator.data = user.username
     if form.validate_on_submit():
-        # Create a new pending study guide and fill in data using input from forms
         new_pending_guide = PendingStudyGuide(
             Class=form.Class.data,
             UnitTopic=form.UnitTopic.data,
@@ -125,123 +88,71 @@ def share():
             Creator=form.Creator.data,
             Link=form.Link.data
         )
-        # Add the new pending study guide to the database
         db.session.add(new_pending_guide)
-        # Commit the changes to the study guide
         db.session.commit()
-        # Redirect to the market home page after sharing
         return redirect(url_for('market_bp.market_home'))
-    # Render share.html for the users to successfully share
-    return render_template('share.html', form=form, user=current_user)
+    return render_template('share.html', form=form, user=user)
 
 
 @market_bp.route('/account', methods=['GET', 'POST'])
-@login_required
+@jwt_required()
 def account_home():
-    """
-    account_home is the user's account page, which displays account info such as wallet balance and cart. This function
-    gives users the option to remove study guides, finalize their cart purchases, and view items in their inventory.
-    """
-    # Initialize the form
+    user = User.query.filter_by(id=get_jwt_identity()).first()
     form = FlaskForm()
-    # Fetch current user's cart
-    cart = current_user.cart
-    # Query user's cart database to get all the items
+    cart = user.cart
     if cart:
         cart_items = CartItem.query.filter_by(cart_id=cart.id).all()
     else:
         cart_items = []
-    # Check for a request in account.html
     if request.method == 'POST':
-        # Get the item_id from the form
         item_id = request.form.get('item_id')
-        # Check if the item_id is valid
         if item_id:
-            # Get the item_id of the study guide in the cart
             cart_item = CartItem.query.get(item_id)
-            # Check if the cart_item exists, and whether the current user's id matches the user_id stored in cart_item
-            if cart_item and cart_item.cart.user_id == current_user.id:
-                # Delete the cart_item from the database
+            if cart_item and cart_item.cart.user_id == user.id:
                 db.session.delete(cart_item)
-                # Commit changes to the database
                 db.session.commit()
-        # Redirect to the account page after removed
         return redirect(url_for('market_bp.account_home'))
 
-    # Fetch all the user's inventory items from their inventory database
-    inventory_items = Inventory.query.filter_by(user_id=current_user.id).all()
-    # Render the account.html template (Setting wallet, cart_items, inventory_items, and the form to what is necessary)
-    return render_template('account.html', wallet=current_user.wallet, cart_items=cart_items, inventory=inventory_items,
-                           form=form)
+    inventory_items = Inventory.query.filter_by(user_id=user.id).all()
+    return render_template('account.html', wallet=user.wallet, cart_items=cart_items, inventory=inventory_items, form=form)
 
 
 @market_bp.route('/finalize_purchase', methods=['POST'])
-@login_required
+@jwt_required()
 def finalize_purchase():
-    """
-    finalize_purchase allows users to finish adding the items in their cart to their inventory, and accordingly checks
-    and subtracts from wallet balance. If the wallet_balance is high enough, the transaction is completed and items are
-    moved to their inventory. Otherwise, it does not go through.
-    """
-    # Fetch the current user's cart
-    cart = current_user.cart
-    # Calculate the total cost of all the items in the cart
+    user = User.query.filter_by(id=get_jwt_identity()).first()
+    cart = user.cart
     total_cost = sum(item.study_guide.Price * item.quantity for item in cart.items)
-    # Check if the user does not have enough money in their wallet
-    if current_user.wallet < total_cost:
-        # Redirect to the account page if they don't have enough money
+    if user.wallet < total_cost:
         return redirect(url_for('market_bp.account_home'))
-    # For each item in the cart
     for item in cart.items:
-        # Get the corresponding study guide from the item
         study_guide = item.study_guide
-        # Find the creator using info from the study guide
         creator = User.query.filter_by(username=study_guide.Creator).first()
-        # If the creator exists, add the price of the study guide to their wallet
         if creator:
             creator.wallet += study_guide.Price * item.quantity
-        # For each item in the cart
         for _ in range(item.quantity):
-            # Create a new inventory item for the user
-            inventory_item = Inventory(user_id=current_user.id, study_guide_id=study_guide.id)
-            # Add the inventory item to the database
+            inventory_item = Inventory(user_id=user.id, study_guide_id=study_guide.id)
             db.session.add(inventory_item)
 
-    # Subtract the total cost from the user's wallet
-    current_user.wallet -= total_cost
-
-    # Delete all items in the cart
+    user.wallet -= total_cost
     CartItem.query.filter_by(cart_id=cart.id).delete()
-
-    # Delete the cart itself
     db.session.delete(cart)
-    # Commit changes to the database
     db.session.commit()
 
-    # Redirect to the account page
     return redirect(url_for('market_bp.account_home'))
 
 
 @market_bp.route('/admin', methods=['GET', 'POST'])
-@login_required
+@jwt_required()
 def admin_home():
-    """
-    Admin home page. Displays pending study guides and allows admin to approve or reject them.
-    """
-    # Check if the user is an admin. If not, redirect to the market home page (is_admin is a boolean in user database)
-    if not current_user.is_admin:
+    user = User.query.filter_by(id=get_jwt_identity()).first()
+    if not user.is_admin:
         return redirect(url_for('market_bp.market_home'))
-    # Initialize form
     form = AdminForm()
-    # Check for form validation
     if request.method == 'POST' and form.validate_on_submit():
-        # Get the action and guide_id from the form (Found in the hidden input fields in admin.html)
         action = request.form.get('action')
-        # Receive the guide_id from the form
         guide_id = int(request.form.get('guide_id'))
-        # Check if the action is approve
         if action == 'approve':
-            # Create a new approved study guide using data from the pending study guide
             approved_guide = PendingStudyGuide.query.get(guide_id)
             if approved_guide:
                 new_guide = StudyGuide(
@@ -251,116 +162,82 @@ def admin_home():
                     Creator=approved_guide.Creator,
                     Link=approved_guide.Link
                 )
-                # Add the new study guide to the database
                 db.session.add(new_guide)
-                # Delete the old study guide from the database
                 db.session.delete(approved_guide)
-                # Commit changes to the database
                 db.session.commit()
         elif action == 'reject':
-            # Reject a pending study guide
             rejected_guide = PendingStudyGuide.query.get(guide_id)
-            # Delete the rejected study guide from the database
             db.session.delete(rejected_guide)
-            # Commit changes to the database
             db.session.commit()
-        # Redirect to the admin page
         return redirect(url_for('market_bp.admin_home'))
-    # Retrieve all pending study guides
     pending_guides = PendingStudyGuide.query.all()
-    # Render admin.html for the admin to approve or reject study guides
-    return render_template('admin.html', pending_guides=pending_guides, user=current_user, form=form)
+    return render_template('admin.html', pending_guides=pending_guides, user=user, form=form)
 
 
 @market_bp.route('/search', methods=['GET', 'POST'])
-@login_required
+@jwt_required()
 def search():
-    """
-    Page for users to search study guides. Users enter a string in searchbar.html and the string is retrieved and stored
-    """
-    # Initialize form
+    user = User.query.filter_by(id=get_jwt_identity()).first()
     form = SearchForm()
-    # Check for form validation
     if form.validate_on_submit():
-        # Get the query from the form
         query = form.query.data
-        # Redirect to the results page using the query
         return redirect(url_for('market_bp.results', query=query))
-    # Render searchbar.html for the users to search for study guides
-    return render_template('searchbar.html', form=form, user=current_user)
+    return render_template('searchbar.html', form=form, user=user)
 
 
 @market_bp.route('/search/results', methods=['GET', 'POST'])
-@login_required
+@jwt_required()
 def results():
-    """
-    results allows users to view the results of their search query, and add study guides to their cart
-    """
-    # Initialize form
+    user = User.query.filter_by(id=get_jwt_identity()).first()
     form = FlaskForm()
-    # Check whether the request went through
     if request.method == 'POST':
-        # Get the action from the form
         action = request.form.get('action')
-        # Check if the action is add_to_cart in results.html (Obtained from clicking the "add-to-cart" button
         if action == 'add_to_cart':
-            # Get the study guide id from the form
             study_guide_id = request.form.get('study_guide_id')
-            # Check if the study guide id is valid
             if study_guide_id:
-                # Get the study guide from the database
                 study_guide = StudyGuide.query.get(study_guide_id)
-                # Check if the study guide exists
                 if study_guide:
-                    # Check if the user has a cart.
-                    if not current_user.cart:
-                        # If the user does not have a cart, create a new cart
-                        cart = Cart(user_id=current_user.id)
-                        # Add the cart to the database
+                    if not user.cart:
+                        cart = Cart(user_id=user.id)
                         db.session.add(cart)
-                        # Commit changes to the database
                         db.session.commit()
-                    # If the user has a cart, get the cart
                     else:
-                        cart = current_user.cart
-                    # Check if the item is already in the cart
+                        cart = user.cart
                     cart_item = CartItem.query.filter_by(cart_id=cart.id, study_guide_id=study_guide_id).first()
-                    # If the item is in the cart, do nothing
                     if cart_item:
                         cart_item.quantity += 0
-                    # If the item is not in the cart, add the item to the cart
                     else:
-                        # Create a new cart item
                         cart_item = CartItem(cart_id=cart.id, study_guide_id=study_guide_id, quantity=1)
-                        # Add the cart item to the database
                         db.session.add(cart_item)
-                    # Commit changes to the database
                     db.session.commit()
-    # Get the query from the form
     query = request.args.get('query')
-    # Get the study guides from the database using the query
     if query:
-        # Get the study guides from the database by filtering using the query
         results = StudyGuide.query.filter(
             (StudyGuide.Class.ilike(f'%{query}%')) |
             (StudyGuide.UnitTopic.ilike(f'%{query}%')) |
             (StudyGuide.Creator.ilike(f'%{query}%'))
         ).all()
     else:
-        # If there is no query, set results to an empty list
         results = []
-
-    # Render the results.html template for the users to view the results of their search query
     return render_template('results.html', query=query, results=results, form=form)
 
 
 @market_bp.route('/logout')
-@login_required
+@jwt_required()
 def logout():
-    """
-    This function logs the user out of the website, ending their session.
-    """
-    # Log the user out
+    # Log the logout event
+    user_id = get_jwt_identity()
+    logger.info(f'User {user_id} is logging out.')
+
+    # Log out the user from Flask-Login session
     logout_user()
-    # Redirect to the home page
-    return redirect(url_for('home'))
+    logger.info(f'User {user_id} has been logged out of Flask-Login.')
+
+    # Prepare the response
+    response = redirect(url_for('home'))
+
+    # Unset JWT cookies from the response
+    unset_jwt_cookies(response)
+    logger.info(f'JWT cookies cleared for user {user_id}.')
+
+    return response
